369077125053fee2f65fab65ca1aa1681bb11040
Anmol Sadanand <anmol.sadanand@outlook.com>
2019-12-12 01:49:56 +0100

    added files

--- Weekend/main.cpp
+++ Weekend/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include "sphere.h"
-#include "hitablelist.h"
-#include "camera.h"
+
 #include "float.h"
+#include "camera.h"
+#include "hitablelist.h"
+#include "material.h"
+#include "sphere.h"
 
-inline double random_double() {
-    return rand() / (RAND_MAX + 1.0);
-}
 
-vec3 color(const ray& r, hitable *world) {
+vec3 color(const ray& r, hitable *world, int depth) {
 
     hit_record rec;
-    if(world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if(world->hit(r, 0.001, FLT_MAX, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * color(scattered, world, depth + 1);
+        }
+        else
+            return vec3(0, 0, 0);
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -37,10 +42,13 @@ int main() {
     vec3 vertical(0., 2., 0.);
     vec3 origin(0., 0., 0.);
 
-    hitable *list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    hitable *world = new hitable_list(list, 2);
+    hitable *list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+
+    hitable *world = new hitable_list(list, 4);
     camera cam;
 
     for(int j = ny - 1; j >= 0; --j) {
@@ -51,11 +59,11 @@ int main() {
                  float v = float(j + random_double()) / float(ny);
                  ray r = cam.get_ray(u, v);
                  vec3 p = r.point_at_parameter(2.);
-                 col+= color(r, world);
+                 col+= color(r, world, 0);
             }
 
             col /= float(ns);
-
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); // Gamma Correction term {gamma 2}
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
