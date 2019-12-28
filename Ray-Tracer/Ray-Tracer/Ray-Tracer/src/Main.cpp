f34c7a50ebcecce4540bd45d56ed1b8bd1c4e376
Anmol Sadanand <anmol.sadanand@outlook.com>
2019-12-28 02:04:03 +0100

    added motion blur

--- Ray-Tracer/Ray-Tracer/Ray-Tracer/src/Main.cpp
+++ Ray-Tracer/Ray-Tracer/Ray-Tracer/src/Main.cpp
@@ -9,19 +9,19 @@
 
 
 hitable *random_scene() {
-	int n = 500;
+	int n = 50000;
 	hitable **list = new hitable*[n + 1];
 	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
 	int i = 1;
 	
-	for (int a = -11; a < 11; ++a) {
-		for (int b = -11; b < 11; ++b) {
+	for (int a = -10; a < 10; ++a) {
+		for (int b = -10; b < 10; ++b) {
 			float choose_mat = random_double();
 			vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
 				
 				if (choose_mat < 0.8) { // Diffuse
-					list[i++] = new sphere(center, 0.2, new lambertian(
+					list[i++] = new moving_sphere(center, center + vec3(0, 0.5*random_double(), 0), 0., 1.0, 0.2, new lambertian(
 						vec3(random_double() * random_double(), random_double() * random_double(), random_double() * random_double()))
 					);
 				}
@@ -64,8 +64,8 @@ vec3 color(const ray& r, hitable *world, int depth) {
 }
 
 int main() {
-    int nx = 1200;
-    int ny = 800;
+    int nx = 200;
+    int ny = 100;
     int ns = 10;
     std::ofstream outfile_ppm;
     outfile_ppm.open("C:\\Users\\anmol\\Desktop\\Scripts\\Cpp\\Projects\\RTT\\Ray-Tracer\\Ray-Tracer\\Ray-Tracer\\res\\Render.ppm");
@@ -76,9 +76,9 @@ int main() {
     
 	vec3 lookFrom(13, 2, 3);
 	vec3 lookAt(0, 0, 0);
-	float dist_to_focus = (lookFrom - lookAt).length();
-	float aperture = 0.1;
-	camera cam(lookFrom, lookAt, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+	float dist_to_focus = 10.0; //(lookFrom - lookAt).length();
+	float aperture = 0.0;
+	camera cam(lookFrom, lookAt, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus, 0.0, 1.0);
 
     for(int j = ny - 1; j >= 0; --j) {
         for(int i = 0; i < nx; ++i) {
--- Ray-Tracer/Ray-Tracer/Ray-Tracer/src/camera.h
+++ Ray-Tracer/Ray-Tracer/Ray-Tracer/src/camera.h
@@ -16,7 +16,10 @@ vec3 random_in_unit_disk() {
 
 class camera {
 public:
-    camera(vec3 lookFrom, vec3 lookAt, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) { // vfov = vertical field of view in degrees
+    camera(vec3 lookFrom, vec3 lookAt, vec3 vup, float vfov, float aspect, float aperture, float focus_dist, float t0, float t1) { // vfov = vertical field of view in degrees
+		
+		time0 = t0;
+		time1 = t1;
 		lens_radius = aperture / 2;
 
 		float theta = vfov * M_PI / 180;
@@ -36,7 +39,8 @@ public:
     ray get_ray(float s, float t) { 
 		vec3 rd = lens_radius * random_in_unit_disk();
 		vec3 offset = u * rd.x() + v * rd.y();
-		return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset); 
+		float time = time0 + random_double()*(time1 - time0);
+		return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset, time); 
 	}
 
     vec3 origin;
@@ -45,6 +49,7 @@ public:
     vec3 vertical;
 	vec3 u, v, w; // define orthonormal camera basis
 	float lens_radius;
+	float time0, time1; // variables for shutter open/ close times
 };
 
 #endif // CAMERAH
